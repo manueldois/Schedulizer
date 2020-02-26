97a903050cb10118c4886b8c8bdc1f38e6581edd
Manuel <manuelsilvadois@gmail.com>
2020-02-26 17:04:13 +0000

    Improved Areas and Tasks lists

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -5,6 +5,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.scss']
 })
 export class SchedulePage implements OnInit {
+  areas = [
+    {name: 'Area 1', id: '1'},
+    {name: 'Area 2', id: '2'},
+    {name: 'Area 3', id: '3'}
+  ]
+  area_active = '2'
 
   constructor() { }
 
